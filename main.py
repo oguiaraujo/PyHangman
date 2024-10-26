be52d7f5dab057658b11cece2084b451b6d13e54
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-10-25 22:06:17 -0300

    feat: Add Reading of the .json File

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import json
 from modules import screens
 
 opt = ""
 
 while opt != "0":
   opt = screens.menu_select_theme()
+
+  with open("./files/words.json") as archive:
+    read = archive.read()
+    themes = json.loads(read)
