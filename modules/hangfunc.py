70740353d27311f82ae4fb28c0046dda8acaa564
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-10-27 16:24:03 -0300

    feat: Create Function game

--- modules/hangfunc.py
+++ modules/hangfunc.py
@@ -1,5 +1,47 @@
 import random
+import os
 
 def get_random_word(dict, theme):
     random_word = random.choice(dict["themes"][theme])
     return random_word.upper()
+
+def game(word):
+    found_letters = ["_" for _ in word]
+    wrong_letters = []
+    attemps = 6
+    while attemps > 0:
+        os.system("clear || cls")
+        print(" ".join(found_letters))
+        print()
+        print("Wrong letters: ", " ".join(wrong_letters))
+        print()
+        print("Remaining chances:", attemps)
+        print()
+        attemp = input("Get the letter: ").upper()
+
+        if attemp in word:
+            index = 0
+            for letter in word:
+                if attemp == letter:
+                    found_letters[index]= letter
+                index += 1
+            if "_" not in found_letters:
+                os.system("clear || cls")
+                print()
+                print(" ".join(found_letters))
+                print()
+                print("Congratulations! You completed the word!")
+                print()
+                input("<ENTER> to back")
+                break
+        else:
+            attemps -= 1
+            wrong_letters.append(attemp)
+            if attemps == 0:
+                os.system("clear || cls")
+                print()
+                print("The word was:", word)
+                print()
+                print("Too bad, you were 'hanged'! Good luck next time!")
+                print()
+                input("<ENTER> to back")
