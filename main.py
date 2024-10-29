4cc71633547bf426a061cbc5edd7a72f54ea0333
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-10-28 23:19:53 -0300

    PyHangman v1.0

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+# Imports header
 import json
 from modules import screens, hangfunc
 
-opt = ""
+#╔═══════-PYHANGMAN-═══════╗
+#║                         ║
+#║     PyHangman v1.0      ║
+#║                         ║
+#║  A classic hangman game ║
+#║  developed in Python,   ║
+#║  showcasing skills in   ║
+#║  JSON file handling.    ║
+#║                         ║
+#║  Author: José Guilherme ║
+#║  Silva de Araújo        ║
+#║                         ║
+#║  Educational Project    ║
+#║  Data Science Academy   ║
+#║                         ║
+#╚═════════════════════════╝
 
+opt = ""
 while opt != "0":
   opt = screens.menu_select_theme()
-
+  # Read the JSON file
   with open("./files/words.json") as archive:
     read = archive.read()
     themes = json.loads(read)
 
     if opt == "1":
-      theme = "Animals"
-      word = hangfunc.get_random_word(themes, theme)
-      hangfunc.game(word)
+      theme = "Animals" # Define the theme corresponding to the option
+      word = hangfunc.get_random_word(themes, theme) # Store the result of the word draw according to the theme
+      hangfunc.game(word) # Run the game based on the selected word
 
     if opt == "2":
       theme = "Countries"
