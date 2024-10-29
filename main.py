451153f451b88dc2e9ca50a7a0eed941d9aa9b89
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-10-28 22:54:23 -0300

    feat: Implement Function game in the "Animals" Theme Selection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from modules import screens
+from modules import screens, hangfunc
 
 opt = ""
 
@@ -9,3 +9,8 @@ while opt != "0":
   with open("./files/words.json") as archive:
     read = archive.read()
     themes = json.loads(read)
+
+    if opt == "1":
+      theme = "Animals"
+      word = hangfunc.get_random_word(themes, theme)
+      hangfunc.game(word)
